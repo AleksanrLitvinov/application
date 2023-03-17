8a5d1c12854ea2e9e731978ab11ad3f783a53e32
Zebra <git@github.com:AleksanrLitvinov/application.git>
2023-03-17 12:33:23 +0700

    первый коммит

---
+++ src/lesson_2/Main.java
@@ -0,0 +1,32 @@
+package lesson_2;
+
+public class Main {
+    public static void main(String[] args) {
+        double a = 60;
+        double b = 45;
+        double c = 17;
+        double P = a + b + c;
+        System.out.println(P);
+//        Math.abs()
+
+        int hungryTime = 9;
+        int fullTime = 15;
+        int bannan = 3;
+        int d = hungryTime/bannan;
+        int s = fullTime/bannan;
+        System.out.println(d +" " + s);
+
+        byte meterA = 127;
+        byte m = 2;
+        System.out.println(meterA + m);
+
+        double dre = 5.0;
+        double rew = 2.0;
+        double wed = dre/rew;
+        System.out.println(wed);
+
+        boolean comparison = 10 > 5;
+        System.out.println(comparison);
+
+    }
+}
